75b64caf6cb58d2a181cf73254c30b00201a73bc
Tamy Moreno <tamy.moreno@ital-partners.com>
2021-05-27 14:09:00 -0500

    mejorando estilos tabla product v1

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,18 +6,85 @@ import {
     Link,
   } from "react-router-dom";
 
-const products = () => {
+const Products = () => {
 
+    const [product, setProduct]=useState([]);
+    const [modalEdit, setModalEdit] = useState(false);
+    const [productEdit, setProductEdit] = useState({
+        id :'',
+		name :'',
+		category :'',
+		price : '',
+		status :'',
+    })
 
+    const ListProductGet = async()=>{
+        const data = await fetch('http://localhost:6039/findAllProducts');
+        const product = await data.json();
+        console.log(product);
+        setProduct(product);
+     };
 
+    const selectProduct = (elemento, caso) => {
+        //console.log(elemento);
+        setProductEdit(elemento);
+        (caso ==='Editar')&&setModalEdit(true)
+    };
 
-
+    
+    useEffect(() => {
+        ListProductGet();
+    }, []);
 
     return (
-        <div>
-            
+        <div className="App">
+            <h1>Products</h1>
+            <br />
+            <div    >
+                <div className="row">
+                    <div className="col-md-10">
+
+                    </div>
+                    <div className="col-md-2">
+                        <Link to="/newOrder" className="btn btn-primary float-left">Create Product</Link>
+                    </div>
+                </div>
+                <br />
+            </div>
+            <hr />
+            <div>
+            <table className="table">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th scope="col">N°</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Categorry</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            product.map((elemento, i) => (
+
+                                <tr key={elemento.id}>
+                                    <th scope="row">{i}</th>
+                                    <td>{elemento.custumer}</td>
+                                    <td>{elemento.condition}</td>
+                                    <td>{elemento.date}</td>
+                                    <td>{elemento.totalTaxes}</td>
+                                    <td>
+                                        <button className="btn btn-primary" onClick={()=>selectProduct(elemento, 'Editar')}>Edit</button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
 
-export default products
+export default Products
